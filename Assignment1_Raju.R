69e18d00b24ea142c9ef2fa6ffd4619fd4352cf8
adhikariraju1 <adhikariraju1@gmail.com>
2016-10-06 19:55:12 +0200

    Changes
    Changes made 1

--- Assignment1_Raju.R
+++ Assignment1_Raju.R
@@ -1,6 +1,13 @@
 library(dplyr)
 library(ggplot2)
+
 data()
 data(diamonds)
 ? diamonds
 names(diamonds)
+
+hist(diamonds$price)
+
+hist(diamonds$price,
+     main = 'Diamonds at Different Price Levels',
+     xlab = 'The price of the diamonds')
